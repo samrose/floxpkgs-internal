4ca01d48396ebed9642694e0a0231e0b790d6892
Tom Bereknyei <tomberek@gmail.com>
2022-05-16 23:46:18 -0400

    fix: simpler python template

---
+++ templates/python3/flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.capacitor.url = "git+ssh://git@github.com/flox/capacitor?ref=ysndr";
+  inputs.capacitor.inputs.root.follows = "/";
+
+  nixConfig.bash-prompt = "[flox]\\e\[38;5;172mλ \\e\[m";
+
+  outputs = _:
+  let lib = import (_.capacitor + "/lib/customisation.nix") _.capacitor _;
+  in
+    _.capacitor _ ({auto, ...}: {
+      devShells = p: auto.using {
+        default = ./flox.toml;
+      } p.pkgs.unstable;
+    });
+}
