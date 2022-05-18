d68d1c8504da11eab1819f4883f9046990843ed3
Michael Brantley <brantley@floxdev.com>
2022-05-18 21:17:14 +0000

    flox-installers: fix syntax error

--- pkgs/flox-installers/default.nix
+++ pkgs/flox-installers/default.nix
@@ -8,7 +8,7 @@ in runCommand "flox-installers.${version}" { } ''
   cp ${nix-installers.rpm.outPath} $out/flox-${version}.rpm
   cp ${nix-installers.pacman.outPath} $out/flox-${version}.pacman
   touch $out/nix-support/hydra-build-products
-  echo "file binary-dist \"$out/flox-${version}.deb\" >> $out/nix-support/hydra-build-products
-  echo "file binary-dist \"$out/flox-${version}.rpm\" >> $out/nix-support/hydra-build-products
-  echo "file binary-dist \"$out/flox-${version}.pacman\" >> $out/nix-support/hydra-build-products
+  echo "file binary-dist \"$out/flox-${version}.deb\"" >> $out/nix-support/hydra-build-products
+  echo "file binary-dist \"$out/flox-${version}.rpm\"" >> $out/nix-support/hydra-build-products
+  echo "file binary-dist \"$out/flox-${version}.pacman\"" >> $out/nix-support/hydra-build-products
 ''
