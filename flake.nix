5a2068528fc7a57c6137b46a634c217c795f768b
Tom Bereknyei <tomberek@gmail.com>
2022-05-19 11:33:53 -0400

    feat: add catalog top-level

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@ rec {
   inputs.nix-installers.url = "git+ssh://git@github.com/flox/nix-installers?ref=flox-hacks";
   inputs.nix-installers.flake = false;
 
+  # This is a misnomer, it's is nixpkgs w/stabilties
   inputs.nixpkgs.url = "git+ssh://git@github.com/flox/nixpkgs-flox";
   inputs.nixpkgs.inputs.capacitor.follows = "capacitor";
   inputs.capacitor.inputs.nixpkgs.follows = "nixpkgs";
@@ -20,87 +21,111 @@ rec {
   inputs.flox.url = "path:./pkgs/flox";
   inputs.catalog.url = "path:./pkgs/catalog";
 
-  outputs = _:
-    (_.capacitor _ ({
-        self,
-        lib,
-        auto,
+  outputs = _: (_.capacitor _ ({
+      self,
+      lib,
+      auto,
+      ...
+    }:
+    # Define package set structure
+    rec {
+      # Limit the systems to fewer or more than default by ucommenting
+      # __systems = ["x86_64-linux"];
+
+      packages = args @ {
+        system,
+        stability ? "unstable",
         ...
       }:
-      # Define package set structure
-      rec {
-        # Limit the systems to fewer or more than default by ucommenting
-        # __systems = ["x86_64-linux"];
+        (legacyPackages (args // {inherit stability;})).flox;
 
-        legacyPackages = {
-          root',
-          pkgs,
-          system,
-          stability ? "unstable",
-          ...
-        }: {
-          nixpkgs = pkgs.${stability};
-          flox =
-            # Tie-the-knot recursive update required in capacitor
-            let
-              tie = (
-                lib.recursiveUpdate
-                root'.legacyPackages.nixpkgs
-                root'.legacyPackages.flox
-              );
-            in
-              # support default.nix approach
-              (auto.automaticPkgsWith inputs ./pkgs tie)
-              # support flakes approach with override
-              // (lib.sanitizes (lib.callSubflakesWith inputs {}) ["default" "packages" system])
-              # External proto-derivaiton trees and overrides
-              // (lib.using (import ./flox.nix {_=_;}) (tie // {inherit inputs;}))
-            # end customizations
-            ;
-        };
+      legacyPackages = {
+        root',
+        pkgs,
+        system,
+        stability ? "unstable",
+        ...
+      }: {
+        nixpkgs = pkgs.${stability};
+        flox =
+          # Tie-the-knot recursive update required in capacitor
+          let
+            tie = lib.recursiveUpdate root'.legacyPackages.nixpkgs root'.legacyPackages.flox;
+          in
+            # support default.nix approach
+            (auto.automaticPkgsWith inputs ./pkgs tie)
+            # support flakes approach with override
+            # searches in "inputs" for a url with "path:./" and call the flake with the root's lock
+            // (lib.sanitizes (lib.callSubflakesWith inputs {}) ["default" "packages" system])
+            # External proto-derivaiton trees and overrides
+            // (lib.using (import ./flox.nix {_ = _;}) (tie // {inherit inputs;}))
+          # end customizations
+          ;
+      };
 
-        # Create output jobsets for stabilities
-        # TODO: has.stabilities and re-arrange attribute names to make system last?
-        hydraJobsRaw = {lib,system,pkgs,...}@args:
-          lib.genAttrs ["stable" "unstable" "staging"] (
-            stability:
-            let jobs =
+      # Create output jobsets for stabilities
+      # TODO: has.stabilities and re-arrange attribute names to make system last?
+      hydraJobsRaw = {
+        lib,
+        system,
+        pkgs,
+        ...
+      } @ args:
+        lib.genAttrs ["stable" "unstable" "staging"] (
+          stability: let
+            jobs =
               (legacyPackages (args // {inherit stability;})).flox;
-            in jobs // {
-              gate = pkgs.runCommand "all-jobs" rec {
-                _hydraAggregate = true;
-                constituentList = lib.collect lib.isList (
-                  lib.mapAttrsRecursiveCond
+          in
+            jobs
+            // {
+              gate =
+                pkgs.runCommand "all-jobs" rec {
+                  _hydraAggregate = true;
+                  constituentList = lib.collect lib.isList (
+                    lib.mapAttrsRecursiveCond
                     (value: !(lib.isDerivation value))
                     (path: value: path)
-                    jobs);
-                constituents = with builtins;
+                    jobs
+                  );
+                  constituents = with builtins;
                     map (x: concatStringsSep "." ([system] ++ x)) constituentList;
-              } ''
-                touch $out
+                } ''
+                  touch $out
                 '';
             }
-          );
-        hydraJobs = a: (_.self.hydraJobsRaw a);
-        hydraJobsStable = lib.genAttrs ["x86_64-linux"] (system: self.hydraJobs.${system}.stable);
-        hydraJobsUnstable = lib.genAttrs ["x86_64-linux"] (system: self.hydraJobs.${system}.unstable);
-        hydraJobsStaging = lib.genAttrs ["x86_64-linux"] (system: self.hydraJobs.${system}.staging);
+        );
 
-        templates = {
-          python-black = {
-            path = ./templates/python-black;
-            description = "Python Black example template";
-          };
-          python2 = {
-            path = ./templates/python2;
-            description = "Python 2 template";
-          };
-          python3 = {
-            path = ./templates/python3;
-            description = "Python 3 template";
-          };
+      catalog =
+        (_.capacitor.lib.project _ ({...}: {
+          legacyPackages = args @ {system, ...}:
+            lib.genAttrs ["flox"] (channel:
+              lib.genAttrs ["stable" "unstable" "staging"] (
+                stability:
+                  (legacyPackages (args // {inherit stability;})).${channel}
+              ));
+        }))
+        .legacyPackages;
+
+      hydraJobs = a: (_.self.hydraJobsRaw a);
+      hydraJobsStable = lib.genAttrs ["x86_64-linux"] (system: self.hydraJobs.${system}.stable);
+      hydraJobsUnstable = lib.genAttrs ["x86_64-linux"] (system: self.hydraJobs.${system}.unstable);
+      hydraJobsStaging = lib.genAttrs ["x86_64-linux"] (system: self.hydraJobs.${system}.staging);
+
+      templates = {
+        python-black = {
+          path = ./templates/python-black;
+          description = "Python Black example template";
         };
-      }));
+        python2 = {
+          path = ./templates/python2;
+          description = "Python 2 template";
+        };
+        python3 = {
+          path = ./templates/python3;
+          description = "Python 3 template";
+        };
+      };
+    }));
 
   # API calls
   inputs.cached__nixpkgs-stable__x86_64-linux.url = "https://hydra.floxsdlc.com/channels/nixpkgs/stable/x86_64-linux.tar.gz";
--- flox.nix
+++ flox.nix
@@ -1,4 +1,5 @@
 {_}: {
+  # External proto-derivation trees
   nix-installers = _.nix-installers + "/default.nix";
   python3Packages = _.floxpkgsv1 + "/pythonPackages";
 }
