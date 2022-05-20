b63e6b7724f6eb38262281cff7506352fb1a3fea
samrose <samuel.rose@gmail.com>
2022-05-20 16:10:02 -0400

    pkg/catalog update changes (#9)

--- pkgs/catalog/flake.nix
+++ pkgs/catalog/flake.nix
@@ -11,6 +11,6 @@
           pname = "catalog";
           version = "1.0.0-r${toString src.revCount}";
           inherit src;
-          vendorSha256 = "sha256-ma0WKBCvnAS6UsPpDHJ2dpmx6UP4G9ng16It/q/A9As=";
+          vendorSha256 = "sha256-3/7jk7SoxBndIoDmXToPwpYUffTXraQOAMzymPhsBZk=";
         });
 }
