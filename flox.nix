1450037e4fef2c77221c83a99478a9608d9e8dbc
Tom Bereknyei <tomberek@gmail.com>
2022-05-24 02:49:17 -0400

    fix: only need python-apt in python3Packages

--- flox.nix
+++ flox.nix
@@ -1,6 +1,5 @@
 {_}: {
   # External proto-derivation trees
   nix-installers = _.nix-installers + "/default.nix";
-  python2Packages = _.floxpkgsv1 + "/pythonPackages";
   python3Packages = _.floxpkgsv1 + "/pythonPackages";
 }
