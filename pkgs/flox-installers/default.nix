cb5e354157305d4de90dfafc4685fd59145df242
Michael Brantley <limeytexan@users.noreply.github.com>
2022-06-04 13:15:08 +0100

    flox-installers: add nix-installers tarball output to binary-dist

--- pkgs/flox-installers/default.nix
+++ pkgs/flox-installers/default.nix
@@ -7,8 +7,10 @@ in runCommand "flox-installers.${version}" { } ''
   cp ${nix-installers.deb.outPath} $out/flox-${version}.deb
   cp ${nix-installers.rpm.outPath} $out/flox-${version}.rpm
   cp ${nix-installers.pacman.outPath} $out/flox-${version}.pacman
+  cp ${nix-installers.tarball.outPath} $out/flox-${version}.tarball
   touch $out/nix-support/hydra-build-products
   echo "file binary-dist \"$out/flox-${version}.deb\"" >> $out/nix-support/hydra-build-products
   echo "file binary-dist \"$out/flox-${version}.rpm\"" >> $out/nix-support/hydra-build-products
   echo "file binary-dist \"$out/flox-${version}.pacman\"" >> $out/nix-support/hydra-build-products
+  echo "file binary-dist \"$out/flox-${version}.tarball\"" >> $out/nix-support/hydra-build-products
 ''
